231bea90290c65f7ffb2c127cfffb727fe4688f2
Your Name <nkeyani-@student.42barcelona.com>
2023-09-10 04:11:21 +0200

    charjoin

---
+++ src/utils/utils.c
@@ -0,0 +1,26 @@
+#include "../../inc/minishell.h"
+
+char  *charjoin(char *s1, char c)
+{
+  int i;
+  char *result;
+  
+  i = 0;
+  if (!s1)
+  {
+    s1 = malloc(sizeof(char) * 1);
+    if (!s1)
+      return (NULL);
+    s1[0] = '\0';
+  }
+  result = malloc(sizeof(char) * (ft_strlen(s1) + 1 + 1));
+  while(s1[i])
+  {
+    result[i] = s1[i];
+    i++;
+  }
+  result[i++] = c;
+  result[i] = '\0';
+  free(s1); 
+  return (result);
+}
