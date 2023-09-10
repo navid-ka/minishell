c4adb0d7769d613f41e927dbb99f87854e7c5e8d
Francisco Javier Costa Ferrer-Vidal <fcosta-f@car2s1.42barcelona.com>
2023-09-10 04:09:57 +0200

    beginning of lexer to clean spaces

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,1 +1,44 @@
 #include "../../inc/minishell.h"
+
+void	clean_spaces(char **line)
+{
+	bool	dcuote;
+	bool	scuote;
+	char	str;
+
+	dcuote = false;
+	scuote = false;
+	while (**line)
+	{
+		if (!dcuote && !scuote)
+		{
+			if (**line != ' ')
+				ft_strjoinchr(str, **line);
+			if (**line == '"')
+				!dcuote;
+			else if (**line == '\'')
+				!scuote;
+			++(*line);
+		}
+		else {
+			if (dcuote)
+			{
+				while (**line != '\"')
+					{
+						ft_strjoinchr(str, **line);
+						*line++;
+					}
+				!dcuote;
+			}
+			else
+			{
+				while (**line != '\'')
+				{
+					ft_strjoinchr(str, **line);
+					*line++;
+				}
+				!scuote;
+			}
+		}
+	}
+}
