b59952bcaddd514ab19d85f8c281301e77772435
Bifrost <nkeyani-@student.42barcelona.com>
2023-09-14 02:13:23 +0200

    teleprompter v2 👅👅👅

--- src/main.c
+++ src/main.c
@@ -17,22 +17,30 @@ void prompter(void)
   pr("\n\n\x1b[0m");
 }
 
-char *shell_prompt(void)
-{
-  char *pwd;
-  char *prompt;
-  char *ansi;
-  char *prompt_left;
-  char *prompt_right;
+char *shell_prompt(void) {
+    char *pwd;
+    char *prompt;
+    char *ansi;
+    char *prompt_left;
+    char *prompt_right;
+    int i;
 
-  pwd = NULL;
-  ansi = "\033[0;36m\033[1m";
-  prompt_left = " 🔞👅 Concha( ͡° ͜ʖ ͡°)\n▸\033[0m";
-  prompt_right = ft_strjoin(ansi, getcwd(pwd, 0));
-  prompt = ft_strjoin(prompt_right, prompt_left);
-  free(prompt_right);
-  free(pwd);
-  return (prompt);
+    pwd = NULL;
+    ansi = "\033[0;36m\033[1m";
+    pwd = getcwd(pwd, 0);
+    i = ft_strlen(pwd) - 1;
+    while (i >= 0 && pwd[i] != '/')
+        i--;
+    if (i >= 0)
+        prompt_left = &pwd[i + 1];
+    else 
+        prompt_left = pwd;
+    prompt_right = ft_strjoin(ansi, prompt_left);
+    prompt_left = "\033[0m 🔞👅 \x1b[35mConcha( ͡° ͜ʖ ͡°)\n▸ \033[0m";
+    prompt = ft_strjoin(prompt_right, prompt_left);
+    free(prompt_right);
+    free(pwd);
+    return (prompt);
 }
 
 void free_tokens(t_token *tok)
@@ -46,7 +54,6 @@ void free_tokens(t_token *tok)
 		tok = tok->next;
 		free(tmp);
 	}
-
 }
 
 int	main(int argc, char **argv, char **env)
@@ -57,11 +64,10 @@ int	main(int argc, char **argv, char **env)
 	char *line;
   char *prompt;
 
-
   prompt = shell_prompt();
   signals();
   prompter();
-
+  line = NULL;
   while (1)
 	{
 		line = readline(prompt);
@@ -69,22 +75,14 @@ int	main(int argc, char **argv, char **env)
 			syntax_error();
 		if (ft_strlen(line) > 0)
             add_history(line);
-		/*if (!ft_strcmp(line, "pwd"))
-			bt_pwd();*/
-		//bt_get_dirs(env, &hola);
 		line = clean_input(line);
 		t_token *tok = get_tokens(line);
 		if (!ft_strcmp(tok->str, "echo"))
 			bt_echo(tok->next);
-		// while(tok)
-		// {
-		// 	printf("str: %s\n type:%d\n\n", tok->str, tok->type);
-		// 	tok = tok->next;
-		// }
 		free_tokens(tok);
-    	//signal(SIGINT, sigint_handler);
+    //signal(SIGINT, sigint_handler);
+    free(line);
 	}
-	free(line);
 	return (0);
 }
 
