e6d542016613fa21df0df7ce163f3f364b52ddf9
Navid Keyani <63590940+navid-ka@users.noreply.github.com>
2023-09-18 23:12:49 +0200

    Update ft_substr.c

--- libs/libft/src/strings/ft_substr.c
+++ libs/libft/src/strings/ft_substr.c
@@ -17,7 +17,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*ptrs;
 	size_t	i;
 
-	if (len == 0) return (NULL);
+	if (len == 0) 
+		return (NULL);
 	if (start >= ft_strlen(s))
 		return (ft_strdup(""));
 	if (start + len > ft_strlen(s))
