c9539ef40b3ff12526621e3ca5ea05cf5bfd2564
Bifrost <nkeyani-@student.42barcelona.com>
2023-09-21 19:24:52 +0200

    testings

--- src/parser/parse_env.c
+++ src/parser/parse_env.c
@@ -14,7 +14,7 @@ int	pos_chr(const char *s, int c)
 	return (-1);
 }
 
-t_env *add_env(char *key, char *value)
+t_env *append_env(char *key, char *value)
 {
   t_env *ls;
 
@@ -31,14 +31,19 @@ void get_env(char **env)
   char *kv[2];
   int i;
   int eq_pos;
+  //t_env *dict;
  
+  //ft_memset(&dict, 0, sizeof(t_env));
   i = ~0;
+  //dict->capacity = ~0;
   while (env[++i])
   {
     eq_pos = pos_chr(env[i], '=');
     kv[0] = ft_strndup(env[i], eq_pos);
     kv[1] = ft_strndup(env[i], ft_strlen(env[i]) - eq_pos + 1);
-    add_env(kv[0], kv[1]);
+    //while (dict->capacity++)
+      append_env(kv[0], kv[1]);
   }
+  //dict->ms_env = ft_split(*env, '\n'); //TODO: find another logic for this implementation.
 }
 
