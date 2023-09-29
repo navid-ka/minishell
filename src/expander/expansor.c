6d65e6d4222dd5996b6135e82a9598bbba3f83b9
Your Name <nkeyani-@student.42barcelona.com>
2023-09-29 15:12:04 +0200

    new get_env

--- src/expander/expansor.c
+++ src/expander/expansor.c
@@ -6,11 +6,25 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 12:04:43 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/09/28 12:23:53 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/09/29 14:12:32 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
+
+//brace expansor https://www.gnu.org/software/bash/manual/html_node/Brace-Expansion.html
+//$a{b,c,d}e
+//expected abe ace ade
+
+//arithmetic expansor https://www.gnu.org/software/bash/manual/html_node/Arithmetic-Expansion.html
+//b=2*3
+//echo $((b))
+//expected 6
+
+//tilde expansor https://www.gnu.org/software/bash/manual/html_node/Tilde-Expansion.html
+// esta no hace falta
+//sh-3.2$ ~+
+//sh: /Users/nkeyani-/test: is a directory
 
 //int count_expand(t_mch *sh, char *token, int *len);
 //void expansor()....
--- src/system/minishell.c
+++ src/system/minishell.c
@@ -6,7 +6,7 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 10:39:00 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/09/28 18:04:13 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/09/29 14:59:30 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ void	free_tokens(t_token *tok)
 	}
 }
 
-static void	sh_init(char **env)
+static void	sh_init(t_mch *sh, char **env)
 {
-	get_env(env);
+	get_env(sh, env);
 	prompter();
 	signals();
 }
@@ -44,7 +44,7 @@ void	minishell(t_mch *sh, char **env)
 	t_token	*tok;
 	(void)sh;
 
-	sh_init(env);
+	sh_init(sh, env);
 	tok = NULL;
 	line = NULL;
 	while (1)
