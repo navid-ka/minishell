a544e785a2c99bf54db82d7293f5fb7445afef8b
Bifrost <nkeyani-@student.42barcelona.com>
2023-10-16 11:39:25 +0200

    cd finished

--- src/builtins/bt_init.c
+++ src/builtins/bt_init.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 11:13:51 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/10/15 01:07:00 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/10/15 23:44:43 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ void  bt_check_builtin(t_mch *sh)
 	t_parser *cmd;
 
 	cmd = sh->parser;
-	/*if (ft_strcmp(argv[0], "cd") == 0)
-		//bt_cd(argv, env);*/
+	if (ft_strcmp(cmd->args[0], "cd") == 0)
+		bt_cd(sh, cmd->args);
 	if (ft_strcmp(cmd->args[0], "env") == 0)
 		bt_env(sh);
 	if (ft_strcmp(cmd->args[0], "echo") == 0)
