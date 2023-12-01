fd99ad087518d2cf841b6fef97e586e089a6b687
Bifrost <nkeyani-@student.42barcelona.com>
2023-12-01 14:52:50 +0100

    fixed builtins with redirections v2

--- src/executor/childs.c
+++ src/executor/childs.c
@@ -6,7 +6,7 @@
 /*   By: bifrost <bifrost@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 12:19:59 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/12/01 14:39:40 by bifrost          ###   ########.fr       */
+/*   Updated: 2023/12/01 14:50:38 by bifrost          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,29 +32,30 @@ void	child(t_parser *top, t_pipe *pipex, t_mch *all)
 
 void	child_pipes(t_parser *top, t_pipe *ptop, t_mch *all)
 {
-    t_pipe		*pipex;
-    t_parser	*pars;
+	t_pipe		*pipex;
+	t_parser	*pars;
 
-    pipex = ptop;
-    pars = top;
-    close(pipex->tube[0]);
-    if (pars->next)
-    {
-        dup2(pipex->tube[1], STDOUT_FILENO);
-    }
-    if (bt_is_builtin(pars->args))
-    {
-        exec_bt(all, pars);
-        exit(all->exit);
-    }
-    else
-    {
-        pipex->file_path = get_args(pars, pipex->routes);
-        execve(pipex->file_path, pars->args, pipex->routes);
-        exit(all->exit);
-    }
-    if (!pars->next)
-        close(pipex->tube[1]);
+	pipex = ptop;
+	pars = top;
+	close(pipex->tube[0]);
+	if (pars->next)
+	{
+		dup2(pipex->tube[1], STDOUT_FILENO);
+		close(pipex->tube[1]);
+	}
+	else
+		close(pipex->tube[1]);
+	if (bt_is_builtin(pars->args))
+	{
+		exec_bt(all, pars);
+		exit(all->exit);
+	}
+	else
+	{
+		pipex->file_path = get_args(pars, pipex->routes);
+		execve(pipex->file_path, pars->args, pipex->routes);
+		exit(all->exit);
+	}
 }
 
 int	wait_childs(t_pipe *pipe, t_mch *all)
